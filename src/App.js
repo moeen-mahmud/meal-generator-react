0faebb0f10ae9f8780b8d106225ce03b2aaa5722
Moeen Mahmud <write2fahim007@gmail.com>
2021-09-19 19:32:05 +0600

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+// const URL = "https://www.themealdb.com/api/json/v1/1/random.php";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={clickHandle}>Click</button>
+      <Meal></Meal>
     </div>
   );
+}
+
+function Meal() {
+  const [meal, setMeal] = useState([]);
+  useEffect(() => {
+    fetch(clickHandle)
+      .then((res) => res.json())
+      .then((data) => setMeal(data.meals[0]));
+  }, []);
+
+  return (
+    <div className="wrapper">
+      <div className="left-side">
+        <figure>
+          <img src={meal.strMealThumb} alt="" />
+          <figcaption>
+            <p>Category: {meal.strCategory}</p>
+            <p>Origin: {meal.strArea}</p>
+          </figcaption>
+        </figure>
+      </div>
+      <div className="right-side"></div>
+    </div>
+  );
+}
+
+function clickHandle() {
+  const URL = "https://www.themealdb.com/api/json/v1/1/random.php";
+  return URL;
 }
 
 export default App;
